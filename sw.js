12eab7cbba3401386d50fa9f9e39cb063528fb1b
20060722llsh <2072174762@qq.com>
2024-10-28 12:18:11 +0800

    Site updated: 2024-10-28 12:18:11

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1730046134659";
+const VERSION = "1730089068504";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
